9c2884f3041f01d23c8f1f8fcc6f3511bf6ce8d7
dreambo8563 <dreambo8563@outlook.com>
2018-11-27 15:24:40 +0800

    doc update

--- main.go
+++ main.go
@@ -14,18 +14,18 @@ func main() {
 
 	r := gin.Default()
 	r.POST("/api/auth/login", func(c *gin.Context) {
-		type UID struct {
+		var reqParams struct {
 			ID string `json:"id"  binding:"required"`
 		}
-		var json UID
-		if err := c.ShouldBindJSON(&json); err != nil {
+		// var reqParams UID
+		if err := c.ShouldBindJSON(&reqParams); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"msg": err.Error(),
 			})
 			return
 		}
 		// 此处默认生成uid过程
-		token, err := jwt.New(json.ID)
+		token, err := jwt.New(reqParams.ID)
 		if err != nil {
 			fmt.Println(err.Error())
 			c.JSON(http.StatusBadRequest, gin.H{

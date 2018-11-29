24d8b5bf5ef7f6a43cf3d54d87c09a0a578c2317
dreambo8563 <dreambo8563@outlook.com>
2018-11-29 12:20:53 +0800

    integrate zap log package

--- controllers/jwt.go
+++ controllers/jwt.go
@@ -3,10 +3,16 @@ package controllers
 import (
 	"net/http"
 
+	"go.uber.org/zap"
+
+	"vincent.com/auth/services/logger"
+
 	"vincent.com/auth/services/jwt"
 
 	"github.com/gin-gonic/gin"
 )
+
+var log = logger.Logger
 
 // JWTNewTokenHandler - new token handler
 func JWTNewTokenHandler(c *gin.Context) {
@@ -21,14 +27,17 @@ func JWTNewTokenHandler(c *gin.Context) {
 		})
 		return
 	}
+	log.Sugar().Infow("get reqParams", "params", reqParams)
 	token, err := jwt.New(reqParams.ID)
 	if err != nil {
 		// jwt err
+		log.Error("generate token err", zap.String("error", err.Error()))
 		c.JSON(http.StatusBadRequest, gin.H{
 			"msg": err.Error(),
 		})
 		return
 	}
+	log.Info("receive token", zap.String("token", token))
 
 	// success
 	c.JSON(http.StatusOK, gin.H{

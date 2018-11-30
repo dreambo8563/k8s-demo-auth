b5bd222af47cc44caab51223efb717ee00489b28
dreambo8563 <dreambo8563@outlook.com>
2018-11-30 15:22:55 +0800

    rm health log

--- controllers/health.go
+++ controllers/health.go
@@ -3,8 +3,6 @@ package controllers
 import (
 	"net/http"
 
-	"go.uber.org/zap"
-
 	"vincent.com/auth/services/logger"
 
 	"github.com/gin-gonic/gin"
@@ -14,7 +12,6 @@ var log = logger.Logger
 
 // HealthCheckHandler - health check handler
 func HealthCheckHandler(c *gin.Context) {
-	log.Info("HealthCheckHandler", zap.String("status", "ok"))
 	// success
 	c.JSON(http.StatusOK, "ok")
 }

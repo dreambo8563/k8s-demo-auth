90383dd2bae4c861e85bdb73640a7376e75653b5
dreambo8563 <dreambo8563@outlook.com>
2018-12-18 19:00:41 +0800

    update proto

--- controllers/health.go
+++ controllers/health.go
@@ -10,7 +10,7 @@ import (
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
-	"vincent.com/auth/rpc/helloworld"
+	"vincent.com/auth/rpc/auth"
 	"vincent.com/auth/services/jwt"
 	"vincent.com/auth/services/logger"
 
@@ -44,7 +44,7 @@ func InitRPCServer(tracer opentracing.Tracer) {
 		otgrpc.OpenTracingServerInterceptor(tracer)),
 		grpc.StreamInterceptor(
 			otgrpc.OpenTracingStreamServerInterceptor(tracer)))
-	helloworld.RegisterGreeterServer(s, &server{})
+	auth.RegisterAuthServiceServer(s, &server{})
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
@@ -53,11 +53,11 @@ func InitRPCServer(tracer opentracing.Tracer) {
 }
 
 // SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloReply, error) {
+func (s *server) GetToken(ctx context.Context, in *auth.GetTokenRequest) (*auth.GetTokenReply, error) {
 	span, childCtx := opentracing.StartSpanFromContext(ctx, "SayHello")
 	defer span.Finish()
-	span.SetTag("UID", in.Name)
-	token, err := jwt.New(childCtx, in.Name)
+	span.SetTag("UID", in.Uid)
+	token, err := jwt.New(childCtx, in.Uid)
 	if err != nil {
 		// jwt err
 		span.LogKV("event", "jwt err", "err", err)
@@ -66,5 +66,5 @@ func (s *server) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*he
 		return nil, err
 	}
 	span.LogKV("event", "jwt success", "token", token)
-	return &helloworld.HelloReply{Message: "Hello " + token}, nil
+	return &auth.GetTokenReply{Token: "Hello " + token}, nil
 }
